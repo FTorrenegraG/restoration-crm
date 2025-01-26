411406b5627dbc43d3e9de3fbd40d8a68db44843
Felix A Torrenegra G <ftorrenegra@lwolf.com>
2025-01-25 22:34:43 -0500

    Refactor user model relationships and password validation
    Removed has_secure_password and password_digest validation
    Adjusted associations to user profiles, assignments, alerts, and ministry roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
-
   has_many :user_profiles, dependent: :destroy
   has_many :assignments, dependent: :destroy
   has_many :alerts, dependent: :destroy
@@ -8,5 +6,4 @@ class User < ApplicationRecord
   has_many :user_ministry_roles, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password_digest, presence: true
 end
