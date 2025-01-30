28a97539ad49dcd05677771fa419d63f438ece04
Felix A Torrenegra G <ftorrenegra@lwolf.com>
2025-01-29 23:48:48 -0500

    Adds ransackable attributes method to Ministry model
    Introduces a method to specify searchable attributes for Ministry model
    Enables searching by attributes like name, description, and timestamps

---
+++ app/admin/ministries.rb
@@ -0,0 +1,53 @@
+ActiveAdmin.register Ministry do
+  # Specify parameters which should be permitted for assignment
+  permit_params :name, :description, :super_ministry
+
+  # or consider:
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :super_ministry]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  # For security, limit the actions that should be available
+  actions :all, except: []
+
+  # Add or remove filters to toggle their visibility
+  filter :name
+  filter :description
+
+  # Add or remove columns to toggle their visibility in the index action
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :super_ministry
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  # Add or remove rows to toggle their visibility in the show action
+  show do
+    attributes_table_for(resource) do
+      row :id
+      row :name
+      row :description
+      row :super_ministry
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  # Add or remove fields to toggle their visibility in the form
+  form do |f|
+    f.semantic_errors(*f.object.errors.attribute_names)
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :super_ministry
+    end
+    f.actions
+  end
+end
--- app/models/ministry.rb
+++ app/models/ministry.rb
@@ -3,4 +3,10 @@ class Ministry < ApplicationRecord
   has_many :user_ministry_roles, dependent: :restrict_with_error
 
   validates :name, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    # Return an array of attribute names that should be searchable
+    # Example:
+    ["created_at", "description", "id", "name", "super_ministry", "updated_at"]
+  end
 end
