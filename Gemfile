71ed2bb95b1e03762a7d91fc34320228a4c094bf
Félix A Torrenegra Gamarra <FTorrenegraG@users.noreply.github.com>
2025-01-29 23:18:06 -0500

    Activeadmin install (#7)
    * Refactor user model relationships and password validation
    
    Removed has_secure_password and password_digest validation
    Adjusted associations to user profiles, assignments, alerts, and ministry roles
    
    * Adds Active Admin for admin panel management
    
    Integrates Active Admin for easy administration tasks
    Includes necessary gems and configurations for Active Admin
    
    * Updates Gemfile dependencies for ActiveAdmin and adds cssbundling-rails gem
    
    Updates ActiveAdmin gem version to '4.0.0.beta15'
    Adds 'cssbundling-rails' gem to Gemfile for CSS bundling functionality
    Updates related dependencies in Gemfile.lock
    
    * Updates Gemfile dependencies for ActiveAdmin and adds cssbundling-rails gem
    
    Updates ActiveAdmin gem version to '4.0.0.beta15'
    Adds 'cssbundling-rails' gem to Gemfile for CSS bundling functionality
    Updates related dependencies in Gemfile.lock
    
    * Adds /app/assets/builds/ to .gitignore
    
    Excludes the /app/assets/builds/ directory from version control to prevent unnecessary tracking of build artifacts
    
    * remove unnecesary file
    
    * Updates TailwindCSS build command for minification
    
    Modifies the build:css script to include minification flag for optimized CSS output
    
    * Add config and db directories to RuboCop ignore list
    
    Expands RuboCop ignore list to include config and db directories for better code styling consistency.
    
    * update gemfile.lock
    
    * Delete node_modules directory
    
    ---------
    
    Co-authored-by: Felix A Torrenegra G <ftorrenegra@lwolf.com>

--- Gemfile
+++ Gemfile
@@ -42,6 +42,19 @@ gem 'thruster', require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+
+gem 'activeadmin', '4.0.0.beta15'
+gem 'sprockets-rails'
+gem 'cssbundling-rails'
+gem 'sassc'
+
+
+# Plus integrations with:
+gem 'devise'
+gem 'cancancan'
+gem 'draper'
+gem 'pundit'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[ mri windows ], require: 'debug/prelude'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
   has_many :user_profiles, dependent: :destroy
   has_many :assignments, dependent: :destroy
   has_many :alerts, dependent: :destroy
@@ -8,5 +10,10 @@ class User < ApplicationRecord
   has_many :user_ministry_roles, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password_digest, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    # Return an array of attribute names that should be searchable
+    # Example:
+    [ 'first_name', 'last_name', 'email', 'created_at' ]
+  end
 end
